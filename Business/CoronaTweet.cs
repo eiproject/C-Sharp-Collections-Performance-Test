07fa92dbe27996febb4b490094996f683472b895
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-08-05 09:07:24 +0700

    Create CoronaTweet object

---
+++ Business/CoronaTweet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsPerformanceTest.Business {
+  class CoronaTweet {
+    private string _csvFilePath;
+    private StreamReader _csvData;
+    internal CoronaTweet() {
+    }
+    internal void Read(string filePath) {
+      _csvFilePath = filePath;
+      _csvData = new StreamReader($@"{ _csvFilePath }");
+    }
+    internal StreamReader GetStreamReader() {
+      return _csvData;
+    }
+  }
+}
