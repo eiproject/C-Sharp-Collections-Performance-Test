7474cfb0382b9cd934f623e70a6776338b896c3e
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-08-05 16:46:44 +0700

    adding ICollections

---
+++ Iterator/TweetsICollection.cs
@@ -0,0 +1,26 @@
+﻿using CollectionsPerformanceTest.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionsPerformanceTest.Business {
+  class TweetsICollection : TweetsIterator {
+    ICollection<Tweet> _tweets; 
+    internal TweetsICollection(ArrayList tweets)
+      : base() {
+      _tweets = new List<Tweet>();
+      foreach (Tweet tweet in tweets) {
+        _tweets.Add(tweet);
+      }
+    }
+
+    internal List<Tweet> GetTweets() {
+      return null;
+    }
+
+    internal override void DoLoop() {
+      foreach(Tweet tweet in _tweets) {
+        // Do Nothing
+      }
+    }
+  }
+}
